24ca5a8f2de6d75b8aad126ecedde0797793ea47
lucasdaniellopes <lucasdanielspead@gmail.com>
2025-09-23 18:06:25 -0300

    add: setup frontend with React + Vite + Typescript + Tailwindcss v4

---
+++ frontend/rota_cultural/src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+
+  return (
+    <>
+    <div className="container w-full bg-black text-white p-4 min-h-screen">
+      <h1>Rota Cultural</h1>
+    </div>
+    </>
+  )
+}
+
+export default App
