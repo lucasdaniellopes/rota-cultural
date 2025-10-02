95895c78b3c3241b08645a734e39ac1afa3445ac
lucasdaniellopes <lucasdanielspead@gmail.com>
2025-10-02 14:38:53 -0300

    feat(backend): add addresses app, model and register at admin

---
+++ backend/rota_cultural/apps/addresses/admin.py
@@ -0,0 +1,21 @@
+from django.contrib import admin
+from .models import Address
+
+@admin.register(Address)
+class AddressAdmin(admin.ModelAdmin):
+    list_display = ["street", "number", "neighborhood", "city"]
+    list_filter = ["neighborhood"]
+    search_fields = ["street", "neighborhood", "city"]
+    ordering = ["neighborhood"]
+
+    fieldsets = (
+        ("Endereço Principal", {
+            "fields": ("street", "number", "complement")
+        }),
+        ("Localização", {
+            "fields": ("neighborhood", "city", "state", "postal_code")
+        }),
+        ("Coordenadas", {
+            "fields": ("point"),
+        }),
+    )
---
+++ backend/rota_cultural/apps/addresses/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from django.contrib.gis.db import models as gis_models
+
+class Address(models.Model):
+    street = models.CharField(max_length=255)
+    number = models.CharField(max_length=20)
+    complement = models.CharField(max_length=255, blank=True, null=True)
+    neighborhood = models.CharField(max_length=100)
+    city = models.CharField(max_length=100)
+    state = models.CharField(max_length=100)
+    postal_code = models.CharField(max_length=20)
+
+    class Meta:
+        verbose_name = "Addresses"
+        ordering = ["neighborhood"]
+
+    def __str__(self):
+        return f"{self.street}, {self.number} -  {self.city}, {self.state}"
