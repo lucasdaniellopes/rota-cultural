7b7d6cfe4e16822dab5b5c0b4eba5b186218e168
José Victor <jose.sv4836@gmail.com>
2025-10-18 18:43:19 -0300

    feat: add Forgot Password page with form and styles

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,7 @@ import {BrowserRouter, Routes, Route, Navigate} from 'react-router-dom'
 import MapPage from '@/pages/MapPage';
 import LoginPage from '@/pages/LoginPage';
 import SignupPage from '@/pages/SignupPage';
+import ForgotPasswordPage from '@/pages/ForgotPasswordPage';
  
 export default function App() {
 
@@ -10,8 +11,9 @@ export default function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/map" element={<MapPage />} />
-         <Route path="/login" element={<LoginPage />} />
+        <Route path="/login" element={<LoginPage />} />
         <Route path="/signup" element={<SignupPage />} />
+        <Route path="/forgot-password" element={<ForgotPasswordPage />} />
         <Route path="/" element={<Navigate to="/login" replace />} />
       </Routes>
     </BrowserRouter>
